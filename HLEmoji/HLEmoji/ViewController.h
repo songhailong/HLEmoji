c821805403f7847516e5231f3975f751c0f46110
songhailong <997496837@qq.com>
2018-08-28 10:26:48 +0800

    初始化

---
+++ HLEmoji/HLEmoji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLEmoji
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/8/28.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLEmoji/HLEmoji/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLEmoji
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/8/28.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
