ace877df10e1372cc8b956d921b9f28149df6ad2
songhailong <997496837@qq.com>
2018-09-13 08:14:28 +0800

    tg

--- HLEmoji/HLEmoji/emoji/View/ExpressionCell.m
+++ HLEmoji/HLEmoji/emoji/View/ExpressionCell.m
@@ -8,14 +8,48 @@
 
 #import "ExpressionCell.h"
 #import "UIImage+Category.h"
+#import "Emoticon.h"
 @implementation ExpressionCell
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
         _ivImg = [[UIImageView alloc]init];
+        _ivImg.frame=CGRectMake(0, 0, 32, 32);
+        _ivImg.center=self.center;
+        
         [self.contentView addSubview:_ivImg];
     }
     return self;
+}
+-(void)setEmoticon:(Emoticon *)emoticon{
+    _emoticon=emoticon;
+    [self updataContent];
+}
+-(void)setIsDelete:(BOOL)isDelete{
+    _isDelete=isDelete;
+    [self updataContent];
+}
+-(void)updataContent{
+    if (_isDelete) {
+        _ivImg.image=[UIImage imageNamed:@"compose_emotion_delete"];
+        
+    }else if (_emoticon){
+        if (_emoticon.type==EmoticonTypeImage) {
+            if (_emoticon.imagePath!=nil) {
+                _ivImg.image=[UIImage imageWithContentsOfFile:_emoticon.imagePath];
+            }else{
+                _ivImg.image=nil;
+            }
+        }else if (_emoticon.type==EmoticonTypeEmoji){
+            if (_emoticon.emotionStr) {
+                UIImage  *image=[UIImage imageWithEmoji:_emoticon.emotionStr size:_ivImg.frame.size.width];
+                _ivImg.image=image;
+            }else
+            {
+               _ivImg.image=nil;
+            }
+        }
+    }
 }
 - (void)loadData:(NSString*)imageName{
     self.ivImg.image=[UIImage imageNamed:imageName];
--- HLEmoji/HLEmoji/emoji/categary/UIImage+Category.m
+++ HLEmoji/HLEmoji/emoji/categary/UIImage+Category.m
@@ -7,9 +7,44 @@
 //
 
 #import "UIImage+Category.h"
-
+//#import <objc/runtime.h>
+//#import <ImageIO/ImageIO.h>
+#import <CoreText/CoreText.h>
+#define CFSTR(cStr)  ((CFStringRef) __builtin___CFStringMakeConstantString ("" cStr ""))
 @implementation UIImage (Category)
-//+(UIImage *)loadExpressionWithPathUrl:(NSString *)PathUrl{
-//
-//}
++(UIImage *)loadExpressionWithPathUrl:(NSString *)PathUrl{
+    if (!PathUrl)return nil  ;
+    //有个内存问题
+    NSString *emoticonBundlePath = [[NSBundle mainBundle] pathForResource:@"Emoticons" ofType:@"bundle"];
+    NSString *emoticonPlistPath = [emoticonBundlePath stringByAppendingPathComponent:PathUrl];
+    UIImage *image=[UIImage imageWithContentsOfFile:emoticonPlistPath];
+    return image;
+}
++ (UIImage *)imageWithEmoji:(NSString *)emoji size:(CGFloat)size {
+    if (emoji.length == 0) return nil;
+    if (size < 1) return nil;
+    
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CTFontRef font = CTFontCreateWithName(CFSTR("AppleColorEmoji"), size * scale, NULL);
+    if (!font) return nil;
+    
+    NSAttributedString *str = [[NSAttributedString alloc] initWithString:emoji attributes:@{ (__bridge id)kCTFontAttributeName:(__bridge id)font, (__bridge id)kCTForegroundColorAttributeName:(__bridge id)[UIColor clearColor].CGColor }];
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef ctx = CGBitmapContextCreate(NULL, size * scale, size * scale, 8, 0, colorSpace, kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedFirst);
+    CGContextSetInterpolationQuality(ctx, kCGInterpolationHigh);
+    CTLineRef line = CTLineCreateWithAttributedString((__bridge CFTypeRef)str);
+    CGRect bounds = CTLineGetBoundsWithOptions(line, kCTLineBoundsUseGlyphPathBounds);
+    CGContextSetTextPosition(ctx, 0, -bounds.origin.y);
+    CTLineDraw(line, ctx);
+    CGImageRef imageRef = CGBitmapContextCreateImage(ctx);
+    UIImage *image = [[UIImage alloc] initWithCGImage:imageRef scale:scale orientation:UIImageOrientationUp];
+    
+    CFRelease(font);
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(ctx);
+    if (line)CFRelease(line);
+    if (imageRef) CFRelease(imageRef);
+    
+    return image;
+}
 @end
