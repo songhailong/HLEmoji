1745e06773b72c47c76d1ba0095c0ddc8e579cf1
songhailong <997496837@qq.com>
2018-09-17 08:29:11 +0800

    gugugyugy

---
+++ HLEmoji/HLEmoji/emoji/View/EmotiocnTextAttachment.m
@@ -0,0 +1,13 @@
+//
+//  EmotiocnTextAttachment.m
+//  HLEmoji
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/9/14.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import "EmotiocnTextAttachment.h"
+
+@implementation EmotiocnTextAttachment
+
+@end
